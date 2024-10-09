b3f24c357bd506b19da6e31b01483ed0d21b5d5b
marcelarie <googlillo@gmail.com>
2024-10-10 00:04:15 +0200

    create functions to get alias contents

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,80 @@ mod syntax_tree;
 
 use std::fs;
 
-use syntax_tree::print_tree;
+// use syntax_tree::print_tree;
 use tree_sitter::Parser;
 
+fn get_raw_string(cursor: &mut tree_sitter::TreeCursor) {
+    let node = cursor.node();
+    let kind = node.kind();
+
+    if kind == "raw_string" {
+        println!("I am a raw_string");
+    }
+
+    if cursor.goto_first_child() {
+        get_raw_string(cursor);
+        while cursor.goto_next_sibling() {
+            get_raw_string(cursor);
+        }
+        cursor.goto_parent();
+    }
+}
+
+fn get_concatenation(cursor: &mut tree_sitter::TreeCursor) {
+    let node = cursor.node();
+    let kind = node.kind();
+
+    if kind == "word" {
+        println!("I am a word");
+
+        if cursor.goto_next_sibling() {
+            get_raw_string(cursor);
+        }
+        cursor.goto_parent();
+    }
+}
+
+fn get_command(cursor: &mut tree_sitter::TreeCursor) {
+    let node = cursor.node();
+    let kind = node.kind();
+
+    if kind == "concatenation" {
+        cursor.goto_first_child();
+        get_concatenation(cursor);
+    }
+
+    // println!("I am a {}", kind);
+
+    if cursor.goto_first_child() {
+        get_command(cursor);
+        while cursor.goto_next_sibling() {
+            get_command(cursor);
+        }
+        cursor.goto_parent();
+    }
+}
+
+fn get_aliases(cursor: &mut tree_sitter::TreeCursor) {
+    let node = cursor.node();
+    let kind = node.kind();
+
+    if kind == "command" {
+        get_command(cursor)
+    }
+
+    // Recursively get aliases
+    if cursor.goto_first_child() {
+        get_aliases(cursor);
+        while cursor.goto_next_sibling() {
+            get_aliases(cursor);
+        }
+        cursor.goto_parent();
+    }
+}
+
 fn main() {
-    const TEST_FILE_PATH: &str = "./src/test/examples/.bash_aliases";
+    const TEST_FILE_PATH: &str = "./src/test/examples/.bashrc";
     let code = fs::read_to_string(TEST_FILE_PATH).expect("Error reading file");
 
     let mut parser = Parser::new();
@@ -26,6 +95,9 @@ fn main() {
 
     let mut cursor = tree.walk();
 
-    println!("Parsed Tree:");
-    print_tree(&mut cursor, code.as_bytes(), 0);
+    // println!("Parsed Tree:");
+    // print_tree(&mut cursor, code.as_bytes(), 0);
+
+    println!("Aliases:");
+    get_aliases(&mut cursor);
 }
