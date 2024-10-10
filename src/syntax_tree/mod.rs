1e04bb4af9fca7fceda2fd6c6e2861c98bf6a931
marcelarie <googlillo@gmail.com>
2024-10-10 18:34:03 +0200

    add tests for validate_nu_tree_sitter_code

--- src/syntax_tree/mod.rs
+++ src/syntax_tree/mod.rs
@@ -9,3 +9,4 @@ pub use printer::print_tree;
 pub use traverser::traverse_tree;
 
 pub use alias::find_aliases;
+pub use nushell::validate_nu_tree_sitter_code;
--- src/syntax_tree/nushell.rs
+++ src/syntax_tree/nushell.rs
@@ -11,3 +11,22 @@ pub fn validate_nu_tree_sitter_code(content: &String) -> bool {
 
     parser.parse(content, None).is_some()
 }
+
+// pub fn validate_nu_parsing(content: &String) -> bool {
+//     // TODO:
+// }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_validate_nu_tree_sitter_code_valid_input() {
+        let valid_nu_code = "alias ll = ls -alF".to_string();
+        let result = validate_nu_tree_sitter_code(&valid_nu_code);
+        assert!(
+            result,
+            "Expected valid Nu code to return true, but got false"
+        );
+    }
+}
