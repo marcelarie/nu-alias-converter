1d0f60f990f787b27645239f6281b48134ac66ec
marcelarie <googlillo@gmail.com>
2024-10-17 16:09:34 +0200

    Improve docs

--- src/config/alias_ignore.rs
+++ src/config/alias_ignore.rs
@@ -2,11 +2,15 @@ use std::collections::HashSet;
 
 use crate::command::arguments::DEBUG_MODE_GLOBAL;
 
+/// Represents the result of parsing the `.aliasignore` file.
+/// * `alias_ignores` - A set of alias names to ignore.
+/// * `command_ignores` - A list of command names to ignore.
 pub struct AliasIgnoreResult {
-    pub alias_ignores: HashSet<String>, // To store alias names to ignore
-    pub command_ignores: Vec<String>, // To store commands from ! that appear inside aliases
+    pub alias_ignores: HashSet<String>, 
+    pub command_ignores: Vec<String>, 
 }
 
+/// Parses the `.aliasignore` file and returns the result.
 pub fn get_ignore_set() -> Option<AliasIgnoreResult> {
     let ignore_file = std::fs::read_to_string(".aliasignore");
     let should_debug = *DEBUG_MODE_GLOBAL.get().unwrap_or(&false);
